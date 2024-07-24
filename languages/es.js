0fa8133dcf88a64cc16be1879048b104f533f89f
eabreu112 <33733685+eabreu112@users.noreply.github.com>
2024-07-24 21:52:20 +0200

    Create es.js
    Language Spanish - Lenguaje Español

---
+++ languages/es.js
@@ -0,0 +1,38 @@
+module.exports = {
+    descriptions: {
+        "N":     "Norte",
+        "NNE":   "Norte-Noreste",
+        "NE":    "Noreste",
+        "ENE":   "Este-Noreste",
+        "E":     "Este",
+        "ESE":   "Este-Sureste",
+        "SE":    "Sureste",
+        "SSE":   "Sur-Surestet",
+        "S":     "Sur",
+        "SSW":   "Sur-Suroeste",
+        "SW":    "Suroeste",
+        "WSW":   "Oeste-Suroeste",
+        "W":     "Oeste",
+        "WNW":   "Oeste-Noroeste",
+        "NW":    "Noroeste",
+        "NNW":   "Norte-Noroeste"
+    },
+    abbreviations: {
+        "N":     "N",
+        "NNE":   "NNE",
+        "NE":    "NE",
+        "ENE":   "ENE",
+        "E":     "E",
+        "ESE":   "ESE",
+        "SE":    "SE",
+        "SSE":   "SSE",
+        "S":     "S",
+        "SSW":   "SSO",
+        "SW":    "SO",
+        "WSW":   "OSO",
+        "W":     "O",
+        "WNW":   "ONO",
+        "NW":    "NO",
+        "NNW":   "NNO"
+     }
+}
